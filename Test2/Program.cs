0dac0504f6baaa0f8ff7c0b39f2f918c47407acd
rybufc <rybufc@gmail.com>
2018-02-17 07:02:28 +0500

    Добавьте файлы проекта.

---
+++ Test2/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    public static class Program
+    {
+        public static void Main()
+        { }
+    }
+
+    public interface IMove
+    {
+        void Process(Field files);
+    }
+
+    public class AddMove : IMove
+    {
+        public Figure figure;
+        public Point point;
+
+        public void Process(Field field)
+        { }
+    }
+
+    public class Figure
+    {
+        public IPlayer Player;
+    }
+
+    public struct Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public class Field
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public List<Figure> Figures;
+
+        public void Add(Point p, Figure f)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Figure f)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Get(Point p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IPlayer
+    {
+        string Name { get; set; }
+
+        IMove CreateMove(Field f);
+    }
+
+    public class Game
+    {
+        private Field field;
+
+        public void MakeMove(IMove m)
+        { }
+    }
+
+    public class GameApplication
+    {
+        public Game CurrentGame;
+        public List<Game> AvailableGames;
+    }
+
+    public class Reversi
+    {
+        private Game game;
+
+        public bool CheckMove(IMove m)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class NetReversiPlayer : IPlayer
+    {
+        public string Name { get; set; }
+        public IMove CreateMove(Field f)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ oop_tests/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_tests
+{
+    public static class Program
+    {
+        public static void Main()
+        {}
+    }
+
+    public class Nucleotide
+    {}
+
+    public class DNA
+    {
+        //A, C, T, G
+        private List<Nucleotide> nucleotides;
+    }
+
+    public class RNA
+    {
+        //A, C, U, G
+        private List<Nucleotide> nucleotides;
+    }
+
+    /// <summary>
+    /// Добавляет аминокислоты к месту синтеза белка(рибосоме)
+    /// </summary>
+    public class TransportRNA : RNA
+    {
+        private AminoAcid aminoAcid;
+    }
+
+    public class AminoAcid
+    { }
+
+    /// <summary>
+    /// Кодирует последовательность белков, по три нуклеотида на аминокислоту
+    /// </summary>
+    public class MatrixRNA : RNA
+    { }
+
+    public class Ribosome
+    {
+        private RibosomeUnit ribosomeUnit1;
+        private RibosomeUnit ribosomeUnit2;
+
+        public Protein Translate(MatrixRNA mrna, IEnumerable<TransportRNA> trna)
+        {
+            return new Protein();
+        }
+    }
+
+    public class Protein
+    {
+        private AminoAcid aminoAcid;
+    }
+
+    public class RibosomeRNA : RNA
+    { }
+
+    public class RNAPolymerase : Protein
+    {
+        public RNA Transcript(DNA dna)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class RibosomeUnit
+    {
+        private List<RibosomeRNA> ribosomeRnas;
+        private List<Protein> proteins;
+    }
+}
